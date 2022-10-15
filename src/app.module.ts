984477558934e69933dec1020860c697b2ddeade
jeehooh <110149590+jeehooh@users.noreply.github.com>
2022-10-15 11:53:10 +0900

    [ADD] 개발 초기세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from 'ormconfig';
+import { ormconfig } from 'ormconfig';
 import { UsersModule } from './users/users.module';
 import { HospitalsModule } from './hospitals/hospitals.module';
 import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule, HospitalsModule, ReservationsModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    UsersModule,
+    HospitalsModule,
+    ReservationsModule,
+  ],
 })
 export class AppModule {}
